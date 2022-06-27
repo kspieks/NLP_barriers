84520233de2ffeba1683396512366c870ce8ad97
Kevin Spiekermann <kspieker@mit.edu>
2022-06-27 09:24:44 -0400

    Add nn_utils

---
+++ rxn_barriers/utils/nn_utils.py
@@ -0,0 +1,38 @@
+import torch.nn as nn
+from transformers import Trainer
+
+
+def param_count(model: nn.Module) -> int:
+    """
+    Determines number of trainable parameters.
+    
+    Args:
+        model: An nn.Module.
+    
+    Returns:
+        The number of trainable parameters.
+    """
+    return sum(param.numel() for param in model.parameters() if param.requires_grad)
+
+
+class CustomTrainer(Trainer):
+    """Create custom loss function to scale or weight the targets"""
+    def __init__(self, scaler=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.scaler = scaler
+
+    def compute_loss(self, model, inputs, return_outputs=False):
+        # feed inputs to model and extract logits
+        outputs = model(**inputs)
+        logits = outputs.get("logits")
+        
+        # extract target Labels
+        labels = inputs.get("labels")
+
+        # define custom loss function with class weights
+        if self.scaler:
+            logits = self.scaler.inverse_transform(logits)
+
+        loss = loss_func(logits, labels)
+        return (loss, outputs) if return_outputs else loss
