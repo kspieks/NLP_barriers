6adc8fca0d9260e78e0aad07f6e0d017056b6d8f
Kevin Spiekermann <kspieker@mit.edu>
2022-06-27 19:25:23 -0400

    Clean up nn_utils.py

--- rxn_barriers/utils/nn_utils.py
+++ rxn_barriers/utils/nn_utils.py
@@ -1,4 +1,5 @@
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import torch
 import torch.nn as nn
 from torch.nn import MSELoss
 from transformers import Trainer
@@ -15,15 +16,6 @@ def param_count(model: nn.Module) -> int:
         The number of trainable parameters.
     """
     return sum(param.numel() for param in model.parameters() if param.requires_grad)
-
-
-def compute_metrics(eval_pred):
-    predictions, labels = eval_pred
-    mae = mean_absolute_error(labels, predictions)
-    rmse = mean_squared_error(labels, predictions, squared=False)
-    R2 = r2_score(labels, predictions)
-
-    return {"MAE": mae, "RMSE": rmse, 'R2': R2}
 
 
 class CustomTrainer(Trainer):
